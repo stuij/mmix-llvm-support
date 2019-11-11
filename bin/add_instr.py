61db1e89d21287de9f113f835c28dd0c932c41c5
Ties Stuij <ties@stuij.se>
2019-11-11 08:51:05 +0000

    rewrite add_instr.py to flexibly print different instruction formats

--- bin/add_instr.py
+++ bin/add_instr.py
@@ -1,93 +1,211 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# This file creates test strings for valid MMIX assembly lit tests.
+# You hand the `print-instr` function an instruction mnemonic, an opcode and
+# a list of operators of varying sizes, and it will construct the appropriate
+# test string. Scaling up, you can create a list of the same opcode class
+# instructions, and print them all out in one go.
+# See the bottom of this file for plenty of examples.
+
 import random
+from enum import Enum, auto
+from functools import reduce
+from io import StringIO
+from operator import add
 
-# print_3op_list(op_list_3)
-op_list_3 = [
-    ["stb", 0xa0],
-    ["stbu", 0xa2],
-    ["stw", 0xa4],
-    ["stwu", 0xa6],
-    ["stt", 0xa8],
-    ["sttu", 0xaa],
-    ["sto", 0xac],
-    ["stou", 0xae],
-]
+def rand(val_size):
+    return random.randint(0, val_size)
 
+def sprintf(buf, fmt, *args):
+    buf.write(fmt.format(*args))
 
-# print_2op_list(op_list_2)
-op_list_2 = [
-    ["seth", 0xe0],
-]
+class OpType(Enum):
+    imm = auto()
+    reg = auto()
+    null = auto()
 
+class Op():
+    def __init__(self, op_type, byte_size=1, val_size=None):
+        self.op_type = op_type
+        self.byte_size = byte_size
+        self.val_size = val_size
+        if not val_size:
+            self.val_size = 2 ** (byte_size * 8)
+        if self.op_type is OpType.null:
+            self.val = 0
+        else:
+            self.val = rand(self.val_size -1)
 
-def rand16():
-    return random.randint(0, 255)
+        val_bytestring = self.val.to_bytes(3, byteorder='little')
+        mv = memoryview(val_bytestring).cast('B')
+        self.val_bytes = [mv[i] for i in range(byte_size -1, -1, -1)]
 
-def rand16_fmt():
-    return format(rand16(), 'x').zfill(2)
+        # assembly print representation
+        if op_type is OpType.reg:
+            self.op_ass = "${0}".format(self.val)
+        else:
+            self.op_ass = "0x" + format(self.val, 'x')
 
-op2 = """# CHECK-INST: {0} ${1},0x{2}{3}
-# CHECK: encoding: [0x{4},0x{5},0x{2},0x{3}]
-# CHECK-DISASS: {4} {5} {2} {3}     {0} ${1},0x{2}{3}
-  {0} ${1},0x{2}{3}"""
+        self.op_hex = ",".join(["0x{0}".format(format(i, 'x').zfill(2)) for i in self.val_bytes])
+        self.op_dis = " ".join(["{0}".format(format(i, 'x').zfill(2)) for i in self.val_bytes])
 
-def print_op2(op, code):
-    reg1_val = rand16()
-    reg1 = "{0}".format(reg1_val)
-    reg1_hex = format(reg1_val, 'x').zfill(2)
-    imm1 = rand16_fmt()
-    imm2 = rand16_fmt()
+# example: print_instr("addi", 0x31, [Op(OpType.reg), Op(OpType.imm, byte_size=2), Op(OpType.imm, val_size=5)])
+def print_instr(instr, code, op_list):
+    assert(reduce(add, [i.byte_size for i in op_list]) <= 3)
 
-    out = op2.format(op, reg1, imm1, imm2, code, reg1_hex)
-    print(out)
+    code_hex = format(code, 'x').zfill(2)
 
-op3 = """# CHECK-INST: {0} ${1},${2},{3}
-# CHECK: encoding: [0x{4},0x{5},0x{6},0x{7}]
-# CHECK-DISASS: {4} {5} {6} {7}     {0} ${1},${2},{3}
-  {0} ${1},${2},{3}"""
+    buf = StringIO()
+    ass_repr = "{0} {1}".format(instr, ",".join([i.op_ass for i in op_list if i.op_type is not OpType.null]))
 
-def print_op3(op, code, regp):
-    reg1_val = rand16()
-    reg1 = "{0}".format(reg1_val)
-    reg1_hex = format(reg1_val, 'x').zfill(2)
+    sprintf(buf, "# CHECK-INST: {0}\n", ass_repr)
 
-    reg2_val = rand16()
-    reg2 = "{0}".format(reg2_val)
-    reg2_hex = format(reg2_val, 'x').zfill(2)
+    ops_hex = ",".join(["{0}".format(i.op_hex) for i in op_list])
+    sprintf(buf, "# CHECK: encoding: [0x{0},{1}]\n", code_hex, ops_hex)
 
-    reg3_val = rand16()
-    reg3 = "${0}".format(reg3_val)
-    reg3_hex = format(reg3_val, 'x').zfill(2)
+    ops_dis = " ".join([i.op_dis for i in op_list])
+    sprintf(buf, "# CHECK-DISASS: {0} {1}   {2}\n", code_hex, ops_dis, ass_repr)
 
-    imm1 = rand16()
-    imm1_bare = format(imm1, 'x').zfill(2)
-    imm1_nofill = "0x" + format(imm1, 'x')
-    imm1_fmt = "0x" + imm1_bare
-
-    nr3 = reg3
-    nr7 = reg3_hex
-    if not regp:
-        nr3 = imm1_nofill
-        nr7 = imm1_bare
-
-    out = op3.format(op, reg1, reg2, nr3, code, reg1_hex, reg2_hex, nr7)
-    print(out)
-
-
-def print_3op_both(op, code):
-    code1 = format(code, 'x').zfill(2)
-    code2 = format(code + 1, 'x').zfill(2)
-    print_op3(op, code1, True)
-    print()
-    print_op3(op, code2, False)
+    sprintf(buf, "  {0}".format(ass_repr))
+    print(buf.getvalue())
 
 def print_list(fn, op_list):
     for i in op_list:
         fn(i[0], i[1])
         print()
 
-def print_3op_list(a_list):
-    print_list(print_3op_both, a_list)
 
-def print_2op_list(a_list):
-    print_list(print_op2, a_list)
+### instruction classes
+
+# leftovers
+# print_list(print_imm_imm_imm, imm_imm_imm_list)
+imm_imm_imm_list = [
+    ["trap", 0x00],
+    ["swym", 0xfd],
+    ["trip", 0xff],
+]
+def print_imm_imm_imm(op, code):
+    print_instr(op, code, [Op(OpType.imm), Op(OpType.imm), Op(OpType.imm)])
+
+def print_irregulars():
+    print_instr("resume", 0xf9, [Op(OpType.null, byte_size=2), Op(OpType.imm)])
+    print()
+    print_instr("save", 0xfa, [Op(OpType.reg), Op(OpType.null, byte_size=2)])
+    print()
+    print_instr("unsave", 0xfb, [Op(OpType.null, byte_size=2), Op(OpType.reg)])
+    print()
+    print_instr("sync", 0xfc, [Op(OpType.null, byte_size=2), Op(OpType.imm)])
+
+## flp3plain, plain floating point
+# print_list(print_flp3plain, flp3plain_list)
+flp3plain_list = [
+    ["fcmp",  0x01],
+    ["fun",   0x02],
+    ["feql",  0x03],
+    ["fadd",  0x04],
+    ["fsub",  0x06],
+    ["fmul",  0x10],
+    ["fcmpe", 0x11],
+    ["fune",  0x12],
+    ["feqle", 0x13],
+    ["fdiv",  0x14],
+    ["frem",  0x16],
+]
+
+def print_flp3plain(op, code):
+    print_instr(op, code, [Op(OpType.reg), Op(OpType.reg), Op(OpType.reg)])
+
+## flp2p5, floating point op with optional y, which atm you always need to specify
+# print_list(print_flp2p5, flp2p5_list)
+flp2p5_list = [
+    ["fix",   0x05],
+    ["fixu",  0x07],
+    ["fsqrt", 0x15],
+    ["fint",  0x17],
+]
+
+def print_flp2p5(op, code):
+    print_instr(op, code, [Op(OpType.reg), Op(OpType.imm, val_size=5), Op(OpType.reg)])
+
+## flp3multi floating point operations can take both reg or imm for z op
+# print_list(print_flp3multi, flp3multi_list)
+flp3multi_list = [
+    ["flot",   0x08],
+    ["flotu",  0x0a],
+    ["sflot",  0x0c],
+    ["sflotu", 0x0e],
+]
+
+def print_flp3multi(op, code):
+    print_instr(op, code, [Op(OpType.reg), Op(OpType.imm, val_size=5), Op(OpType.reg)])
+    print()
+    print_instr(op, code + 1, [Op(OpType.reg), Op(OpType.imm, val_size=5), Op(OpType.imm)])
+    
+## xyz_multi
+# it's a common pattern, mostly used by ALU instrs
+# print_list(print_xyz_multi, xyz_multi_list)
+alu3multi_list = [
+    ["mul", 0x18],
+    ["mulu", 0x1a],
+    ["div", 0x1c],
+    ["divu", 0x1e],
+
+    ["sl", 0x38],
+    ["slu", 0x3a],
+    ["sr", 0x3c],
+    ["sru", 0x3e],
+
+    ["bdif", 0xd0],
+    ["wdif", 0xd2],
+    ["tdif", 0xd4],
+    ["odif", 0xd6],
+    ["mux", 0xd8],
+    ["sadd", 0xda],
+    ["mor", 0xdc],
+    ["mxor", 0xde],
+]
+
+xyz_multi_list = [
+    ["ldsf",  0x90],
+    ["ldht",  0x92],
+    ["ldvts", 0x98],
+    ["stsf",  0xb0],
+    ["stht",  0xb2],
+    ["stco",  0xb4],
+]
+
+def print_xyz_multi(op, code):
+    print_instr(op, code, [Op(OpType.reg), Op(OpType.reg), Op(OpType.reg)])
+    print()
+    print_instr(op, code + 1, [Op(OpType.reg), Op(OpType.reg), Op(OpType.imm)])
+
+## directives
+# print_list(print_di_multi, di_multi_list)
+di_multi_list = [
+    ["preld",  0x9a],
+    ["prego",  0x9c],
+    ["syncd",  0xb8],
+    ["prest",  0xba],
+    ["syncid", 0xbc],    
+]
+
+def print_di_multi(op, code):
+    print_instr(op, code, [Op(OpType.imm), Op(OpType.reg), Op(OpType.reg)])
+    print()
+    print_instr(op, code + 1, [Op(OpType.imm), Op(OpType.reg), Op(OpType.imm)])
+
+## wyde
+# print_list(print_wyde, wyde_list)
+wyde_list = [
+    ["inch", 0xe4],
+    ["incmh", 0xe5],
+    ["incml", 0xe6],
+    ["incl", 0xe7],
+
+    ["andh", 0xec],
+    ["andmh", 0xed],
+    ["andml", 0xee],
+    ["andl", 0xef],
+]
+
+def print_wyde(op, code):
+    print_instr(op, code, [Op(OpType.reg), Op(OpType.imm, byte_size=2)])
